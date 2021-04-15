5f80aedcf57e11da68b3490daa40e758bf046ec2
shernandezf <69701714+shernandezf@users.noreply.github.com>
2021-04-14 19:56:13 -0500

    Add files via upload

---
+++ TypeScritp_taller1/dataCourses.ts
@@ -0,0 +1,9 @@
+import { Course } from './course.js';
+
+export const dataCourses = [
+  new Course("Ingeniería de Sw", "Bocanegra", 3),
+  new Course("Japones 3", "Maria Angélica Otalora", 3),
+  new Course("Sistrans", "German Bravo", 3),
+  new Course("Lenguajes y Maquinas", "Takahashi", 3),
+  new Course("Lineal", "Carolina Benedeti", 6)
+]
---
+++ TypeScritp_taller1/scripts/dataCourses.js
@@ -0,0 +1,8 @@
+import { Course } from './course.js';
+export var dataCourses = [
+    new Course("Ingeniería de Sw", "Bocanegra", 3),
+    new Course("Japones 3", "Maria Angélica Otalora", 3),
+    new Course("Sistrans", "German Bravo", 3),
+    new Course("Lenguajes y Maquinas", "Takahashi", 3),
+    new Course("Lineal", "Carolina Benedeti", 6)
+];
